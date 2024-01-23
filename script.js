277b4e4cfbbb84311ba725c36d800e2d639d8f51
astrahalora <astrahalora@gmail.com>
2024-01-23 15:27:49 +0200

    Add timer class and imports

---
+++ script.js
@@ -0,0 +1,9 @@
+import { Timer } from './assets/js/timer.js';
+
+// DOM elems
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+// Timer
+const myTimer = new Timer(durationInput, startButton, pauseButton);
